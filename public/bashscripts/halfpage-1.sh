63da30e9374eec4540a0a1b6a4b7d7e6222f43d4
tombetthauser <tombetthauser@gmail.com>
2022-05-29 08:57:42 -0700

    Start script overhaul

--- public/bashscripts/halfpage-1.sh
+++ public/bashscripts/halfpage-1.sh
@@ -1,8 +1,55 @@
-set -x
+set -x # logs all commands in the shell
 
-# clear out directories
 
-# set page size
+# ~~~~~ RESET DIRECTORIES ~~~~~ 
+
+#   temp directories
+#   for images and pages
+
+rm -rf ./public/zine-images
+rm -rf ./public/zine-pages
+mkdir ./public/zine-images
+mkdir ./public/zine-pages
+
+
+# ~~~~~ SET ZINE DIMENSIONS ~~~~~ 
+
+#   single side
+#   8.5x11 inch paper
+#   vertically orientation
+
+xZineImageCount=1
+yZineIMageCount=2
+
+
+# ~~~~~ SET ZINE ORIENTATION ~~~~~ 
+
+#   rotation represents zine orientation
+#   first page not always in top left
+#   measured in degrees
+
+pageOneRotation=270 
+
+
+# ~~~~~ SET ZINE RESOLUTION ~~~~~ 
+
+#   determines print resolution
+#   calculates zine image resolution
+#   assumes an 1/8th inch border
+#   on all zine images
+
+#   border percentage represents 1.51%
+#   which is 1/8th inch on an 8 1/4 inch print page
+#   note 1/8th inch border is taken off 8.5 inch print width
+
+xPageSizePixels=850
+yPageSizePixels=1100
+
+xBorderPercent=151 # represents 1.51%
+eightInchPixels=$(( (xPageSixePixels * xBorderPercent) / 10000 ))
+quarterInchPixels=$(( eighthInchPixels * 2 ))
+
+
 # set pages needed array
 
 # set image size
